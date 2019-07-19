e96e4330bee02fc5657dd843b078814f1b1529e1
Esteve Tarrago <tarragoesteve@gmail.com>
2019-07-19 13:14:58 +0200

    tex ajustments

--- software/mechanical_computations/lagrange_4.py
+++ software/mechanical_computations/lagrange_4.py
@@ -7,7 +7,7 @@ from scipy import optimize, integrate
 from robot import Robot
 from tqdm import tqdm
 
-transition_time=0
+transition_time = 0
 
 accumulated_error = 0
 previous_error = 0
@@ -17,8 +17,10 @@ kp = +0.1
 ki = 0.1
 kd = 0.01
 
+
 def nearestMultiple(target_angle, current_angle):
     return target_angle + round((current_angle - target_angle)/(2*math.pi))*2*math.pi
+
 
 def PID(error, time):
     global accumulated_error
@@ -36,8 +38,10 @@ def PID(error, time):
         return kp * error
     else:
         if time - previous_error_time > 0:
-            accumulated_error = accumulated_error + error * (time - previous_error_time)
-            output = kp * error + ki * accumulated_error + kd * (error - previous_error) / (time - previous_error_time)
+            accumulated_error = accumulated_error + \
+                error * (time - previous_error_time)
+            output = kp * error + ki * accumulated_error + kd * \
+                (error - previous_error) / (time - previous_error_time)
         else:
             output = kp * error + ki * accumulated_error
         previous_error = error
@@ -47,13 +51,16 @@ def PID(error, time):
 
 def external_torque(robot, t, q, dot_q):
     global transition_time
-    if t< transition_time:
-        output = PID( math.pi/2 - (q[0]+q[1]+q[2]), t)
-        tau = min(output, max(0,2*robot.max_torque(dot_q[1])), max(0,robot.max_torque(dot_q[2])))
+    if t < transition_time:
+        output = PID(math.pi/2 - (q[0]+q[1]+q[2]), t)
+        tau = min(output, max(
+            0, 2*robot.max_torque(dot_q[1])), max(0, robot.max_torque(dot_q[2])))
         return [0, tau, tau]
     else:
-        tau = max(0,min(2*robot.max_torque(dot_q[1]), robot.max_torque(dot_q[2])))
+        tau = max(
+            0, min(2*robot.max_torque(dot_q[1]), robot.max_torque(dot_q[2])))
         return [0, tau, tau]
+
 
 def system_function(robot: Robot):
     M = numpy.matrix([[robot.I_wheel()+robot.I_platform() + robot.I_flywheel(robot.r_min()) + robot.m_total() * robot.r_wheel**2,
@@ -83,17 +90,17 @@ def system_function(robot: Robot):
 
 my_robot = Robot()
 my_robot.set_r_flywheel_r_wheel_w_N(.086, .10, .04, 2)
-results=[]
-num_divisions = 20
-total_time = 6
-time_divisions = numpy.linspace(0,total_time,num_divisions)
+results = []
+num_divisions = 50
+total_time = 1.5
+time_divisions = numpy.linspace(0, total_time, num_divisions)
 for tt in tqdm(time_divisions):
-    transition_time= tt
+    transition_time = tt
     accumulated_error = 0
     previous_error = 0
     previous_error_time = 0
     first_error = True
-    initial_contition = [0,0,0,0,0,0]
+    initial_contition = [0, 0, 0, 0, 0, 0]
     ode_int = scipy.integrate.solve_ivp(
         system_function(my_robot),
         (0, total_time),
@@ -120,26 +127,31 @@ for i in range(len(results)):
         max_index = i
 
 plt.figure()
-plt.title('Max distance of '+ str(max_distance) + 'at transition-time ' + str(time_divisions[max_index]))
+plt.title('Max distance of ' + str(max_distance) +
+          'at transition-time ' + str(time_divisions[max_index]))
 plt.xlabel('t [s]')
 plt.ylabel('theta [rad]')
 plt.plot(results[max_index].t, results[max_index].y[0])
 plt.plot(results[max_index].t, results[max_index].y[1])
 plt.plot(results[max_index].t, results[max_index].y[2])
 plt.plot(results[max_index].t, results[max_index].y[0]+results[max_index].y[1])
-plt.plot(results[max_index].t, results[max_index].y[0]+results[max_index].y[1]+results[max_index].y[2])
+plt.plot(results[max_index].t, results[max_index].y[0] +
+         results[max_index].y[1]+results[max_index].y[2])
 plt.legend(['q[0]', 'q[1]', 'q[2]', 'ground-platform', 'ground-flywheel'])
 
 
 plt.figure()
-plt.title('Max distance of '+ str(max_distance) + 'at transition-time ' + str(time_divisions[max_index]))
+plt.title('Max distance of ' + str(max_distance) +
+          'at transition-time ' + str(time_divisions[max_index]))
 plt.xlabel('t [s]')
 plt.ylabel('theta dot [rad/s]')
 plt.plot(results[max_index].t, results[max_index].y[3])
 plt.plot(results[max_index].t, results[max_index].y[4])
 plt.plot(results[max_index].t, results[max_index].y[5])
 plt.plot(results[max_index].t, results[max_index].y[3]+results[max_index].y[4])
-plt.plot(results[max_index].t, results[max_index].y[3]+results[max_index].y[4]+results[max_index].y[5])
-plt.legend(['dot_q[0]', 'dot_q[1]', 'dot_q[2]','ground-platform' ,'ground-flywheel'])
+plt.plot(results[max_index].t, results[max_index].y[3] +
+         results[max_index].y[4]+results[max_index].y[5])
+plt.legend(['dot_q[0]', 'dot_q[1]', 'dot_q[2]',
+            'ground-platform', 'ground-flywheel'])
 
 plt.show()
